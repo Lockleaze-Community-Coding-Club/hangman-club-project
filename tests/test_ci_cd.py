b145cd8d3c599e7e28ffcfefca266d5b8acbc842
Roanne Perrin <RoannePerrin@gmail.com>
2025-08-30 19:53:42 +0100

    Attempt to pass Flint8

--- tests/test_ci_cd.py
+++ tests/test_ci_cd.py
@@ -1,13 +1,15 @@
 # tests/test_ci_cd.py
-
 def test_addition():
     assert 2 + 2 == 4
+
 
 def test_string():
     assert "CI/CD" in "Welcome to CI/CD Pipeline"
 
+
 def test_boolean():
     assert True is not False
-    
+
+ 
 def test_subtraction():
     assert 2 - 2 == 4
