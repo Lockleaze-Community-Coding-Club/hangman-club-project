3a399a29b1032a5f6f15d23ce3c110cde6b7be7c
rickhardy <45637470+rickhardy@users.noreply.github.com>
2025-09-07 09:57:58 +0100

    coding standards changes

--- code/lockleaze_hangman.py
+++ code/lockleaze_hangman.py
@@ -5,8 +5,8 @@ word_selected = select_word()
 winner = True
 
 if winner:
-    print('You are correct todays word was: ' , word_selected) 
-else: 
+    print('You are correct todays word was: ', word_selected)
+else:
     print('Loser')
 
 
@@ -14,6 +14,8 @@ else:
 # python code/lockleaze_hangman.py
 
 # it will look like this
-# Rick@LAPTOP-IK2NAICQ MINGW64 /c/Lockleaze Hangman/hangman-club-project (master)
+# Rick@LAPTOP-IK2NAICQ MINGW64 
+# /c/Lockleaze Hangman/hangman-club-project (master)
 # $ python code/lockleaze_hangman.py
-# empt string -- this needs to contain the word chosen and the masked version to display on the screen
+# empt string -- this needs to contain the word 
+# chosen and the masked version to display on the screen
