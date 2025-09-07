1f0901caf6b88aba51a51947581e2cc2f73c9bc6
rickhardy <45637470+rickhardy@users.noreply.github.com>
2025-09-07 09:42:24 +0100

    added some detail of what to do next and a rudimentary top level python file called lockleaze_hangman.py as the main function

---
+++ code/lockleaze_hangman.py
@@ -0,0 +1,19 @@
+
+from select_word import select_word
+
+word_selected = select_word()
+winner = True
+
+if winner:
+    print ('You are correct todays word was: ' , word_selected) 
+else: 
+    print ('Loser')
+
+
+## To run the hangman project from bash use this command:
+## python code/lockleaze_hangman.py
+
+## it will look like this
+## Rick@LAPTOP-IK2NAICQ MINGW64 /c/Lockleaze Hangman/hangman-club-project (master)
+## $ python code/lockleaze_hangman.py
+## empty string -- this needs to contain the word chosen and the masked version to display on the screen
---
+++ code/select_word.py
@@ -0,0 +1,4 @@
+def select_word():
+    # put logic here (see issues) and change the return value
+    return 'empty string -- this needs to contain the word chosen and the masked version to display on the screen'
+
