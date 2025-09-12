cfc92fa2cbfd27a5773c647da3196ce1ece2e815
rickhardy <45637470+rickhardy@users.noreply.github.com>
2025-09-12 19:05:38 +0100

    coding stds

--- tests/test_select_word.py
+++ tests/test_select_word.py
@@ -1,11 +1,14 @@
-import sys
 from select_word import select_word
+
+
 def test_select_word_returns_tupel():
     type_returned_by_select_word = type(select_word())
-    test_tuple=(1,1)
+    test_tuple = (1, 1)
     type_of_test_tupel = type(test_tuple)
     assert type_returned_by_select_word == type_of_test_tupel
-def test_select_word_returns_strings():  
+
+
+def test_select_word_returns_strings():
     word_selected = select_word()
     first_element = word_selected[0]
     second_element = word_selected[1]
@@ -14,10 +17,10 @@ def test_select_word_returns_strings():
     type_of_first_element = type(first_element)
     type_of_second_element = type(second_element)
     assert type_of_first_element == type_of_test_string
-    assert type_of_second_element == type_of_test_string 
+    assert type_of_second_element == type_of_test_string
+
+
 def test_word_same_length_as_template():
     word_selected = select_word()
     assert len(word_selected[0]) == len(word_selected[1])
-
-
-
+    
