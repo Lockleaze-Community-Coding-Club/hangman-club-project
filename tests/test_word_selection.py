820d49c9613cc3b16338c113ef3a9909c6fe4cb9
rickhardy <45637470+rickhardy@users.noreply.github.com>
2025-09-27 18:51:52 +0100

    changed the order of tests

--- tests/test_word_selection.py
+++ tests/test_word_selection.py
@@ -1,14 +1,15 @@
 #This is a test to test the word_selection function
 import sys
 import os
+from re import fullmatch
 
 sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), \
+    os.path.abspath \
+        (os.path.join(os.path.dirname(__file__), \
                              "../code"))
 )
 
 from word_selection import word_selection
-
 
 def test_format_string():
     """Check that the actual return \
@@ -22,9 +23,9 @@ def test_length():
      assert len (word_selection('hello')) < 47
 
 def test_no_space():
-    from re import fullmatch
+    
     """Check that there is only one word supplied \
         and there are no spaces"""
     assert fullmatch("[A-Za-z]+", \
                      word_selection('hello')), \
-          f"String contains non-letter characters: "
+          "String contains non-letter characters: "
