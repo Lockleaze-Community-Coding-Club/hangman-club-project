6e957a2e09217a6ee60d805c79f5bf5f42fce5cb
Roanne <RoannePerrin@gmail.com>
2025-10-11 17:20:37 +0100

    Updated with the choose_word unit tests

--- tests/test_word_selection.py
+++ tests/test_word_selection.py
@@ -12,9 +12,30 @@ def test_input_error():
 def test_parse_words():
     """Check that the words.txt file is parsed as a list of words."""
     result = word_selection.parse_words("test_words.txt")
-    expected = ["animal", "banana", "carrot"]
+    expected = ["animal", "banana", "carrot", "donkey"]
     assert result == expected, f"Expected {expected}, but got {result}"
     # Issue 28
+
+def test_choose_word():
+    """Check that a single word is chosen from the list"""
+    fake_list = ["red","yellow","pink","green"]
+    result = word_selection.choose_word(fake_list)
+    assert result in fake_list, f"Expected one of {fake_list}, but got {result}"
+    #Issue 29
+
+def test_choose_word2():
+    """Check that an error is returned if the list contains no words"""
+    fake_list = []  
+    with pytest.raises(ValueError):
+         word_selection.choose_word(fake_list)
+    #Issue 29
+
+def test_word_selection_randomness():
+    results = {word_selection() for _ in range(100)}
+    # Expect that more than one unique word was selected
+    assert len(results) > 1, f"Word selection not random enough: {results}"
+    #Issue 29
+
 
 def test_format_string():
     """Check that the actual return \
