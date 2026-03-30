e06aec3f7b950c338ad7bb03fb7c0966a1bd971a
Samuel Ewetade <sewetade29@gmail.com>
2026-03-30 21:10:24 +0100

    Inputs amended

--- tests/test_functions_for_play_game/test_guessed_letters.py
+++ tests/test_functions_for_play_game/test_guessed_letters.py
@@ -6,9 +6,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import pytest
 from functions_for_play_game.guessed_letters import used_letters_function
 def test_used_letters_function():
-    letter_list = ["X", "C", "L"]
-    new_text = "U"
-    result = used_letters_function(letter_list, new_text)
+    used_letters = ["X", "C", "L"]
+    letter = "U"
+    result = used_letters_function(used_letters, letter)
        #This will make a list of used letters
     assert result == ["x", "c", "l","u"] 
        #if condition returns true then nothing happens, if false then assertion error is raised
