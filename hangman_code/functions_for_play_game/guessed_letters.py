12cd37814d3775ace3deb00b30c43122b9b921c5
Samuel Ewetade <sewetade29@gmail.com>
2026-04-20 19:25:51 +0100

    Updates to usedletters function. Test shown to work.

--- hangman_code/functions_for_play_game/guessed_letters.py
+++ hangman_code/functions_for_play_game/guessed_letters.py
@@ -1,16 +1,11 @@
-
-def used_letters_function(letter, used_letters):
+def used_letters_function(used_letters, letter):
+   
     used_letters [:]= [item.lower() for item in used_letters]
             
     letter = letter.lower()
     # Step 1: convert to lowercase
     used_letters.append(letter)   # Add text to the end of the list
-    print(used_letters)
     return used_letters
-
-
-
-
 
 
 
