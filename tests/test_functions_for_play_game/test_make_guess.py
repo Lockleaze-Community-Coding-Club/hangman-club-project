d3ee3ab8492930eb69035d75405acd6cd604c2ea
Roanne <RoannePerrin@gmail.com>
2026-05-10 17:12:15 +0100

    turned Make_guess into snake class

--- tests/test_functions_for_play_game/test_make_guess.py
+++ tests/test_functions_for_play_game/test_make_guess.py
@@ -1,12 +1,12 @@
 import pytest
-from hangman_code.functions_for_play_game.make_guess import Make_guess
+from hangman_code.functions_for_play_game.make_guess import make_guess
 from hangman_code.functions_for_play_game.make_guess import remaining_attempts_function
 
 def test_make_guess_fills_all_matching_letters():
     """Test to check that a word with two or more of the same letter fills in
       the guessed word multiple times"""
     test_letter = "A"
-    test_result = Make_guess(
+    test_result = make_guess(
         test_letter,
         ["A", "B", "A", "C", "U", "S"],
         ["_", "_", "_", "_", "_", "_"],
@@ -18,7 +18,7 @@ def test_make_guess_wrong_letter_guess():
     """Test to check that the function behaves as expected when a wrong letter
     is inserted"""
     test_letter = "E"
-    test_result = Make_guess(
+    test_result = make_guess(
         test_letter,
         ["A", "B", "A", "C", "U", "S"],
         ["_", "_", "_", "_", "_", "_"],
@@ -32,7 +32,7 @@ def test_make_guess_different_list_lengths():
     length)"""
     test_letter = "E"
     with pytest.raises(ValueError):
-        Make_guess(
+        make_guess(
         test_letter,
         ["A", "B", "A", "C", "U", "S"],
         [""])
